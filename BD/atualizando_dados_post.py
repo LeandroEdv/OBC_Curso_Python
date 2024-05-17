7ff9773d3aa8c7aa29f48e0a0b432e1223448a73
Leandro Santana <103543245+LeandroEdv@users.noreply.github.com>
2024-05-17 19:51:27 -0300

    Create atualizando_dados_post.py

---
+++ BD/atualizando_dados_post.py
@@ -0,0 +1,13 @@
+from conexao_post import conn
+cursor_obj = conn.cursor()
+
+sql = """ 
+    UPDATE game 
+    SET name = %s
+    WHERE id = %s
+"""
+cursor_obj.execute(sql, ("Fifa 23", 5))
+
+conn.commit()
+print('dados atualizados com sucesso')
+conn.close()
