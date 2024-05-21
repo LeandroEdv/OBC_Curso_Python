3539c153331fb9684dfca50e9e721b671ea9bf4e
Leandro Santana <103543245+LeandroEdv@users.noreply.github.com>
2024-05-21 12:44:03 -0300

    Update conexao_orm.py

--- Gerenciamneto_de_Blog/conexao_orm.py
+++ Gerenciamneto_de_Blog/conexao_orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
