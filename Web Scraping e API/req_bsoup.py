2efa1b2b14e0a26209ba513392515843ab042f87
Leandro Santana <103543245+LeandroEdv@users.noreply.github.com>
2024-06-06 12:58:12 -0300

    Create req_bsoup.py

---
+++ Web Scraping e API/req_bsoup.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=ft&searchTextText=&txtKeywords=python&txtLocation=")
+print(response.status_code)
+#soup = BeautifulSoup(response.text,'lxml') # < - tenho de ver isso do lxml
+#jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
