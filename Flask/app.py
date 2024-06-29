8d25e29454718bdd7e0fafdd3823f50ec06c9009
Leandro Santana <leandro.edv@hotmail.com>
2024-06-29 17:44:20 -0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -13,6 +13,14 @@ def principal():
     
     return render_template("index.html", conteudos = conteudos)
 
-@app.route('/sobre')
-def sobre():
-    return render_template('sobre.html')
+@app.route('/diario')
+def diario():
+    diario = {
+            "Fulano":5.0,
+            "Beltrano":6.0,
+            "Teste":7.0,
+            "Sicrano":8.5,
+            "Rodrigo": 9.0,
+            "Fernanda":10.0
+    }
+    return render_template('sobre.html', diario = diario)
