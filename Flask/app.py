29c936113cb068e24392b60b143b71e75849cb1b
Leandro Santana <leandro.edv@hotmail.com>
2024-07-05 12:24:01 -0300

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -3,9 +3,16 @@
 #
 from flask import Flask, render_template, request
 from lista_filmes import resultado_filmes
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite///livros.sqlite3'
+
+db = SQLAlchemy()
+db.init_app(app)
+
 conteudos = []
 registros = []
 #localhost:5000/
