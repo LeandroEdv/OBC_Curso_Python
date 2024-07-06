4cfab8b54903e7c40590b972ba2ba95517b7e5e0
Leandro Santana <leandro.edv@hotmail.com>
2024-07-06 19:04:58 -0300

    atualizaçao

--- Flask/projeto/routes.py
+++ Flask/projeto/routes.py
@@ -1,5 +1,5 @@
-from projeto import app
-from flask import render_template, request
+from projeto import app, db
+from flask import render_template, request, redirect, url_for
 from projeto.lista_filmes import resultado_filmes
 from projeto.livro import livro
 
@@ -43,7 +43,57 @@ def lista_filmes(propriedade):
     
 @app.route('/livros')
 def lista_livros():
+    page = request.args.get('page', 1, type=int)
+    per_page = 2
+    todos_livros = livro.query.paginete(page=page, per_page=per_page)
     return render_template(
-        "livros.html", 
-        livros=livro.query.all()
+        "livros.html" 
+        , livros=todos_livros
     )
+
+
+@app.route('/add_livro', methods=["GET", "POST"])
+def adiciona_livro():
+    nome = request.form.get('nome')
+    descricao = request.form.get('descricao')
+    valor = request.form.get('valor')
+    
+    if request.method == 'POST':
+        livro_add = livro(
+            nome,
+            descricao,
+            valor
+        )
+        db.session.add(livro_add)
+        db.session.commit()
+        return redirect(url_for('lista_livros'))
+    return render_template("novo_livro.html")
+
+@app.route('//atualiza_livro', methods=["GET", "POST"])
+def atualiza_livro(id):
+    # select * from livro where id = 2
+    livro_bd = livro.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        valor = request.form['valor']
+        
+        livro.query.filter_by(id=id).update({
+            "nome": nome,
+            "descricao": descricao,
+            "valor": valor
+        })
+        
+        db.session.commit()
+        return redirect(url_for('lista_livros'))
+    return render_template(
+        "atualiza_livro.html",
+        livro=livro_bd
+    ) 
+    
+@app.route('//remove_livro')
+def remove_livro(id):
+    livro_bd = livro.query.filter_by(id=id).first()
+    db.session.delete(livro_bd)
+    db.session.commit()
+    return redirect(url_for('lista_livros'))
